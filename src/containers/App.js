ce02e7e92d0108e0a4786c3ba17d5011759f60b8
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2020-03-12 12:56:01 +0900

    single counter

---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Countercontainer from "../containers/CounterContainer";
+
+class App extends Component {
+  render() {
+    return <Countercontainer />;
+  }
+}
+
+export default App;
